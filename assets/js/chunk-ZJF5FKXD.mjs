250eb7fc84475c40e3a2a3489a6ba5c81fe3614f
matthewkyleherren <99541432+matthewkyleherren@users.noreply.github.com>
2024-02-12 15:18:30 +0100

    ergwa

---
+++ assets/js/chunk-ZJF5FKXD.mjs
@@ -0,0 +1,2 @@
+var t=a=>({description:"This is the brand identity portal for B&B Group®.",favicon:new URL("https://framerusercontent.com/images/CUbenRrX7E9yxp99qkRJdZGC6Vo.svg").href,socialImage:new URL("/images/aDd27N5ldgBGEhTvq1CLBExUAmA.jpg").href,title:"B&B Group® | Brand Identity"}),o=t;export{o as a};
+//# sourceMappingURL=chunk-ZJF5FKXD.mjs.map
